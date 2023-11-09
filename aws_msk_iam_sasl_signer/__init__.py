ce1feb77645ad83eea936b14643227296d14c906
Sankalp Bhatia <sankal@amazon.com>
2023-11-09 20:35:18 +0000

    Bump to v1

--- aws_msk_iam_sasl_signer/__init__.py
+++ aws_msk_iam_sasl_signer/__init__.py
@@ -1,5 +1,5 @@
 #  Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #  SPDX-License-Identifier: Apache-2.0
 
-__author__ = "Mohit Paliwal"
-__version__ = "0.1.0"
+__author__ = "Amazon Managed Streaming for Apache Kafka"
+__version__ = "1.0.0"
--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/aws/aws-msk-iam-sasl-signer-python",
-    version="0.1.0",
+    version="1.0.0",
     zip_safe=False,
 )
